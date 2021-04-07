9a6cb1a365e3e4a2058067a4c6bc49c7afba93fe
--global <--global>
2021-04-08 01:55:37 +0500

    version1.1.0

---
+++ script.js
@@ -0,0 +1,25 @@
+var answer=prompt('Enter your name')
+console.log('My name is', answer);
+
+var answer=prompt('Enter your age')
+console.log('I am ', answer);
+
+
+var answer=prompt('Solve the exercise 6+3=?' )
+console.log('Misol 1: 6+3=9', 'Your answer is', answer);
+
+
+var answer=prompt('Solve the exercise 20-5=?' )
+console.log('Misol 2: 20-5=15', 'Your answer is', answer);
+
+var answer=prompt('Solve the exercise 15*2=?' )
+console.log('Misol 3: 15*2=30', 'Your answer is', answer);
+
+var answer=prompt('Solve the exercise 8/4=?' )
+console.log('Misol 3: 8/4=2', 'Your answer is', answer);
+
+var answer=prompt('Solve the exercise 10%3=?' )
+console.log('Misol 3: 10%3=1', 'Your answer is', answer);
+
+ 
+
